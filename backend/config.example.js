c938d6367203c6632051e796671418a7782874b5
Andrew Richardson <awrichardson6@gmail.com>
2020-12-18 12:17:31 -0500

    backend: add example config

---
+++ backend/config.example.js
@@ -0,0 +1,17 @@
+module.exports = {
+  KALEIDO: {
+    contractUrl: "https://abcdef-connect.us0-aws.kaleido.io/instances/123456789?openapi",
+    viewer: {
+      auth: "user:password",
+      address: "0x123456789",
+    },
+    distributor: {
+      auth: "user:password",
+      address: "0x123456789",
+    }
+  },
+  SPOTIFY: {
+    clientId: 'my-id',
+    clientSecret: 'my-secret',
+  },
+};
